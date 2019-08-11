ad2075c34871290e303a3b0d9d9de5752eb87c1e
League Student <league@iMac-16.attlocal.net>
2019-08-11 11:28:27 -0700

    for loop gauntlet ip

---
+++ src/_06_for_loop_gauntlet/forloopgauntlet.java
@@ -0,0 +1,11 @@
+package _06_for_loop_gauntlet;
+
+import javax.swing.JOptionPane;
+
+public class forloopgauntlet {
+public static void main(String[] args) {
+	for (int i = 1; i < 4; i++) {
+		for (int j = 0; j <3; j++) {
+		System.out.println(i+j);	
+		}
+	}}}
