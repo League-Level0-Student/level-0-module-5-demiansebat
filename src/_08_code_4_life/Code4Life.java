55eebb0a30245bf35fd2aa43c497105566f3c3f1
League Student <league@iMac-20.attlocal.net>
2019-08-25 11:24:41 -0700

    code for life, data types 1,2, whats the output , question 10 ip

--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -11,21 +11,34 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
+public static void main(String[] args) {
+	
 
 	// 1. Ask the user how many hours they spent coding this week.
+String work=JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+	int code=Integer.parseInt(work);
+	if (code<=2) {
+		JOptionPane.showMessageDialog(null, "You need to stop watching youtube and code instead.");
+	}
 
-	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
+// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
 
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+	else if(code>=3) {
+		JOptionPane.showMessageDialog(null, "You are a code ninja!");
+	}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
-
+	if (code>5) {
+		playBatmanTheme();
+	}
+	}
 	private static void playBatmanTheme() {
-		try {
+    try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
 			sound.play();
 			Thread.sleep(60002);
@@ -33,5 +46,4 @@ public class Code4Life {
 			ex.printStackTrace();
 		}
 	}
-
-}
+	}
