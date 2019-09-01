ca6999cbd0e11b7d5813aba0d85d448ad243b08a
League Student <league@iMac-27.attlocal.net>
2019-09-01 11:30:06 -0700

    thing

---
+++ src/blank.java
@@ -0,0 +1,10 @@
+
+public class blank {
+		public static void main(String[] args){
+			String count = "";
+			for(int i = 5; i >= 0; i--){
+				count += "count" + i + " ";
+			}
+			System.out.println(count);
+		}
+}
